8c4702e3c0e8bba25e103f16c118b87191b0d98d
Ian <imallarino2015@fau.edu>
2024-08-30 01:08:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,29 @@
 #include<iostream>
 
-#include"include.h"
+#include "encrypt.h"
+#include "base64.h"
+#include "prng.h"
+#include "functions.h"
 
 using namespace std;
 
 int main(int argc,char** argv){
-    string in="",out="output";
-    int key=1;
-    bool validate=false,hex=false;  //various flags
+    string startingStr="";
+    for(unsigned char a=0x20;a<0x7f;a++)
+        startingStr += (char)a;
+    startingStr += "\t\n";
 
-    for(int a=1;a<argc;a++){    //set up program arguments
-        if(argv[a]==(string)"--help"||argv[a]==(string)"-h"){  ///help
-            cout<<"Usage: "<<argv[0]<<" [option] <argument>"<<endl
-                <<"Available options: \n"
-                <<"-i <path>: \t Path/Name of the desired file to encrypt/decrypt \n"
-                <<"-o <path>: \t Path/Name of the resultant file \n"
-                <<"-k <integer>: \t Give the program a starting key \n"
-                <<"-v: \t Verify the correctness of the output files \n"
-                <<"-x: \t Create a hexadecimal representation file \n";
-            return 0;
-        }
-        if(argv[a]==(string)"-k"){  ///-k for key
-            a++;
-            key=toInt(argv[a]);
-            continue;
-        }
-        if(argv[a]==(string)"-i"){  ///-i for the input filename
-            a++;
-            in=argv[a];
-            continue;
-        }
-        if(argv[a]==(string)"-o"){  ///-o for the output filename
-            a++;
-            out=argv[a];
-            continue;
-        }
-        if(argv[a]==(string)"-v"){  ///-v for validation
-            validate=true;
-            continue;
-        }
-        if(argv[a]==(string)"-x"){  ///-x for hex output
-            hex=true;
-            continue;
-        }
-    }
+    string testStr = "";
+    for(unsigned a = 0; a < 1; a++)
+        testStr += startingStr;
 
-    if(processFile(in,out,key)){
-        if(validate){
-            cout<<validateFile(in,out,key)<<endl;
-        }
-        if(hex){
-            outputHex(out,"data/hexOut.txt");
-        }
-    }else{
-        rnd(key);
-        for(unsigned a=0;a<in.size();a++)
-            in[a]^=rnd()%256;
-        cout<<in<<endl;
-    }
+    cout << testStr << endl;
+
+    string processedString;
+    PRNG rng;
+    cout << "------------------------------------------" << endl;
+    encryptTest(testStr, &encrypt::changeStrBin);
+    cout << "------------------------------------------" << endl;
+    
     return 0;   ///TODO: GUI
 }
